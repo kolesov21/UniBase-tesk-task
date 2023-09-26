d92a95c1f0ac0eb6f85ab5764fc01c8c8e27c6a0
Dmiriy <kolesov.fsd@gmail.com>
2023-09-26 15:54:17 +0500

    update

--- practice-1/sctript/dragAndDrop.js
+++ practice-1/sctript/dragAndDrop.js
@@ -1,6 +1,7 @@
 const dropArea = document.querySelector('.file-drop-area');
 const fileInput = document.querySelector('#file');
 const labelText = document.querySelector('.file-drop-area__text');
+const clearIcon = document.querySelector('.file-drop-area__icon');
 
 function preventDefaultBehavior(event) {
     event.preventDefault();
@@ -13,10 +14,32 @@ dropArea.addEventListener('dragover', preventDefaultBehavior);
 dropArea.addEventListener('drop', function(event) {
     preventDefaultBehavior(event);
     const file = event.dataTransfer.files[0];
-    labelText.textContent = 'Успешно! Другой?';
 });
 
-fileInput.addEventListener('change', function() {
-    const file = this.files[0];
-    labelText.textContent = 'Успешно! Другой?';
+fileInput.addEventListener('click', function(event) {
+    preventDefaultBehavior(event);
+    const imageURL = prompt('Укажите ссылку на картинку');
+    isImageURL(imageURL);
+
+    function isImageURL(url) {
+        const img = new Image();
+
+        img.onload = function() {
+          if (img.width > 0 && img.height > 0) {
+            dropArea.style.backgroundImage = `url(${imageURL})`;
+          } else {
+            alert("Ссылка не ведет на изображение.");
+          }
+        };
+      
+        img.onerror = function() {
+          alert("Ошибка при загрузке изображения.");
+        };
+
+        img.src = url;
+    }
 });
+
+clearIcon.addEventListener('click', function(){
+    dropArea.style.backgroundImage = "url(./img/image-example.png)";
+})
